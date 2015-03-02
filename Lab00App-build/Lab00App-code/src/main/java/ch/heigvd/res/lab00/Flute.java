dbe77d3d8515ac851d814dae8c8bc235251c5f05
djeule2 <djeuleze@yahoo.fr>
2015-03-02 15:38:32 +0100

    Rendu

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,16 @@
+package ch.heigvd.res.lab00;
+
+public class Flute implements IInstrument {
+
+ public String play (){
+	 return "pin pong";
+ }
+ 
+ public int getSoundVolume(){
+	 return 2;
+ }
+ 
+ public String getColor(){
+	 return "red";
+ }
+  }
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Trumpet.java
@@ -0,0 +1,16 @@
+
+package ch.heigvd.res.lab00;
+public class Trumpet implements IInstrument{
+
+ public String play (){
+	 return "pouet";
+ }
+ 
+ public int getSoundVolume(){
+	 return 10;
+ }
+ 
+ public String getColor(){
+	 return "golden";
+ }
+  }
